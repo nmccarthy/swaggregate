c3b59bfedcff121a6e2629bdf236dbda2178b077
Neil McCarthy <nmccarthy@yammer-inc.com>
2013-03-18 23:12:12 -0700

    messing with the UI a bit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ class PagesController < ApplicationController
   skip_before_filter :authenticate, :only => :login
 
   def home
+    @subscriptions = Subscription.where(:user_id => current_user.id)
   end
 
   def login
